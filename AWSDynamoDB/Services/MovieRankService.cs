c2a0c715191c179d0fb1ede7b509f0f76c46dcd5
Juan Cortés <juan.cortes@pevaar.com>
2020-11-28 13:32:39 -0500

    Add project files.

---
+++ AWSDynamoDB/Services/MovieRankService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSDynamoDB.Services
+{
+    public class MovieRankService : IMovieRankService
+    {
+    }
+}
